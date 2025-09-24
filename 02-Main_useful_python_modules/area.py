91f33765b376d7e25efec3b06a788e88d0443a5e
dsluse <slusedominique@yahoo.fr>
2025-09-24 20:26:04 +0200

    Add filled Nb for lectures of Sep. 22 and 24th

---
+++ 02-Main_useful_python_modules/area.py
@@ -0,0 +1,21 @@
+import math 
+
+def square(side):
+    '''
+    Area of a square
+    '''
+    return side * side
+
+def circle(radius):
+    '''
+    Area of a circle
+    
+    Parameters:
+    radius (float): Radius of the circle
+    
+    Returns: 
+    Area (float) : aread of the circle
+    '''
+
+    return math.pi * radius**2
+
